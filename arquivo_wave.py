593faf3d0bae3b9fa4aa8629b845f8736b44ca65
vituzm <marqueszenivitor@gmail.com>
2024-08-17 18:23:55 -0300

    Prints de feedback tirados

--- arquivo_wave.py
+++ arquivo_wave.py
@@ -1,23 +1,24 @@
 """
-tentando fazer um arquivo .wave
+Programa para a criação do arquivo wave de acordo com o 
+tempo desejado
 """
 
 import wave
 import serial
 
 # Configurações da serial
-SERIAL_PORT = 'COM3'  # Porta serial
-BAUD_RATE = 115200    # Configuração da taxa de transmissão da serial
+SERIAL_PORT = 'COM9'  # Porta serial
+BAUD_RATE = 921200    # Configuração da taxa de transmissão da serial
 CHUNK = 2             # Tamanho do buffer agora é 2 bytes por vez (16 bits = 2 bytes)
 FORMAT = 16           # Usando 16 bits por amostra (não precisa ser mudado, só é informativo)
 CHANNELS = 1          # Canal mono
 SAMPLE_RATE = 19940   # Taxa de amostragem em Hz
-RECORD_SECONDS = 1    # Tempo de gravação
-WAVE_NAME = "SerialToWave.wav"  # Nome do arquivo de saída
+RECORD_SECONDS = 2    # Tempo de gravação
+WAVE_NAME = "AudioEstetoscópio.wav"  # Nome do arquivo de saída
 
 # Número total de amostras a serem gravadas
-# TOTALSAMPLES = SAMPLE_RATE * RECORD_SECONDS
-TOTALSAMPLES = 20000
+
+TOTALSAMPLES = SAMPLE_RATE * RECORD_SECONDS
 # Criar e configurar o arquivo WAV
 waveform = wave.open(WAVE_NAME, 'wb')
 waveform.setnchannels(CHANNELS)
@@ -27,21 +28,13 @@ waveform.setframerate(SAMPLE_RATE)
 try:
     # Abrir a porta serial com timeout
     port = serial.Serial(SERIAL_PORT, BAUD_RATE)
-    print("Iniciando captura de dados da serial...")
+    print("...Iniciando captura de dados da serial...")
 
     # Ler e gravar dados
     for i in range(TOTALSAMPLES):
         valor_serial = port.read(CHUNK)  # Ler o tamanho de uma amostra completa
-        print(f"Amostra {i+1}/{TOTALSAMPLES} gravada.")
-        """
-        print(valor_serial.decode("windows-1252"))
-        if len(valor_serial) < CHUNK:
-            print("Dados insuficientes recebidos.")
-            break
-        """
         waveform.writeframes(valor_serial)  # Gravar os dados no arquivo WAV
-        print(f"VALOR: {valor_serial} ")
-    print("Captura e gravação finalizadas.")
+    print("___Captura e gravação finalizadas___")
 
 finally:
     port.close()
