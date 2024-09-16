ddc723b6c0504b7f8ace942ebf325f6d2ce99436
Zenni <marqueszenivitor@gmail.com>
2024-09-16 00:32:46 -0300

    IHM, modularização do código e filtro para coração e pulmão

--- crate_wave.py
+++ crate_wave.py
@@ -2,34 +2,39 @@ import wave
 import numpy as np
 import scipy.signal as signal
 
-def create_wave_file(filename, channels, sampwidth, framerate):
+CHUNK = 2             # Tamanho do buffer agora é 2 bytes por vez (16 bits = 2 bytes)
+FORMAT = 16           # Usando 16 bits por amostra (não precisa ser mudado, só é informativo)
+CHANNELS = 1          # Canal mono
+
+def create_wave_file(filename, framerate):
     """Cria e configura um arquivo WAV para gravação."""
     waveform = wave.open(filename, 'wb')
-    waveform.setnchannels(channels)
-    waveform.setsampwidth(sampwidth)
+    waveform.setnchannels(CHANNELS)
+    waveform.setsampwidth(CHUNK)
     waveform.setframerate(framerate)
     return waveform
 
-def apply_filter(input_filename, output_filename, sos, SAMPLE_RATE):
+def apply_filter(input_filename, output_filename, sos):
     """Aplica o filtro e salva o áudio filtrado em um novo arquivo WAV."""
     with wave.open(input_filename, 'rb') as wave_in:
-        channels = 1
-        sample_width = 2
-        framerate = SAMPLE_RATE
         num_frames = wave_in.getnframes()
+        framerate = wave_in.getframerate()
 
-        audio_data = wave_in.readframes(num_frames)
-        audio_int = (np.frombuffer(audio_data, dtype=np.int16))
+        audio_data = wave_in.readframes(num_frames) # Le as amostras de áudio 
+        audio_int = (np.frombuffer(audio_data, dtype=np.int16)) # Coloca para int16 
 
-        filtered_samples = signal.sosfilt(sos, audio_int)
+        filtered_samples = signal.sosfilt(sos, audio_int) # Filtragem do sinal 
 
         # Normalizar os dados filtrados para 16 bits
         filtered_samples = np.clip(filtered_samples, -32768, 32767)
         filtered_samples = filtered_samples.astype(np.int16)
 
     with wave.open(output_filename, 'wb') as wave_out:
-        wave_out.setnchannels(channels)
-        wave_out.setsampwidth(sample_width)
+        wave_out.setnchannels(CHANNELS)
+        wave_out.setsampwidth(CHUNK)
         wave_out.setframerate(framerate)
         wave_out.writeframes(filtered_samples.tobytes())
         print(f"Arquivo filtrado salvo como {output_filename}")
+
+    wave_in.close()
+    wave_out.close()
--- main.py
+++ main.py
@@ -1,41 +1,76 @@
+"""Arquivo principal do projeto."""
 import config_serial 
 import crate_wave 
 import filter_design
+from tkinter import *
 
-# Configurações da serial
-SERIAL_PORT = 'COM3'  # Porta serial
-BAUD_RATE = 460800    # Configuração da taxa de transmissão da serial
-CHUNK = 2             # Tamanho do buffer agora é 2 bytes por vez (16 bits = 2 bytes)
-FORMAT = 16           # Usando 16 bits por amostra (não precisa ser mudado, só é informativo)
-CHANNELS = 1          # Canal mono
 SAMPLE_RATE = 15000   # Taxa de amostragem em Hz
-RECORD_SECONDS = 15   # Tempo de gravação
-input_wave_name  = "C:\\Users\\Vitor\\Downloads\\pulmao.wav"  # Nome do arquivo de saída
-output_wave_name = "C:\\Users\\Vitor\\Downloads\\pulmao FILTRADO.wav"
-TOTALSAMPLES = SAMPLE_RATE * RECORD_SECONDS # Número total de amostras a serem gravadas
-FILTER_BAND = [80, 800]
-SECTIONS = 18
-ATTENUATION_STOPBAND = 80
+RECORD_SECONDS = 15   # Tempo de gravação  
 
+TOTALSAMPLES = SAMPLE_RATE * RECORD_SECONDS # Número total de amostras a serem gravadas
+
+def toggle_state(state):
+    global audio, audio_filtrado, pulmao
+    pulmao = state
+
+    if state:  # Se o estado for True, define os nomes para pulmão
+        audio = "C:\\Users\\Vitor\\Downloads\\pulmao.wav"
+        audio_filtrado = "C:\\Users\\Vitor\\Downloads\\pulmao FILTRADO.wav"
+    else:  # Se o estado for False, define os nomes para coração
+        audio  = "C:\\Users\\Vitor\\Downloads\\coracao.wav"
+        audio_filtrado = "C:\\Users\\Vitor\\Downloads\\coracao FILTRADO.wav"
 
 def main():
+    # Mensagem indicando a gravação 
+    status_label.config(text="Gravando...")
 
     # Criação do arquivo WAV para gravação
-    waveform = crate_wave.create_wave_file(input_wave_name, CHANNELS, CHUNK, SAMPLE_RATE)
+    waveform = crate_wave.create_wave_file(audio, SAMPLE_RATE)
 
     try:
         # Configuração e uso da porta serial
-        port = config_serial.setup_serial(SERIAL_PORT, BAUD_RATE)
-        config_serial.record_from_serial(port, waveform, TOTALSAMPLES, CHUNK)
+        port = config_serial.setup_serial()
+        config_serial.record_from_serial(waveform, TOTALSAMPLES, port)
     finally:
         port.close()
         waveform.close()
 
-    print(f"Arquivo {input_wave_name} criado com sucesso.")
+    print(f"Arquivo {audio} criado com sucesso.")
 
     # Aplicação e salvamento dos filtros
-    SOS = filter_design.design_filter_1(FILTER_BAND, SECTIONS, ATTENUATION_STOPBAND, SAMPLE_RATE)
-    crate_wave.apply_filter(input_wave_name, output_wave_name, SOS, SAMPLE_RATE)
+    SOS = filter_design.design_filter_1(pulmao, SAMPLE_RATE)
+    crate_wave.apply_filter(audio, audio_filtrado, SOS)
+    
+    # Mensagem indicando o fim da gravação e opção de nova gravação
+    status_label.config(text="Gravação concluída! Pronto para uma nova gravação.")
+
+
+# Interface gráfica 
+def create_gui():
+    global status_label
+    window = Tk()
+
+    window.title("Configuração de Filtro")
+    # Label para status da gravação
+    status_label = Label(window, text="Selecione o filtro e inicie a gravação.")
+    status_label.pack(pady=5)
+
+    # Definir tamanho fixo da janela (largura x altura)
+    window.geometry("300x200")
+
+    # Botão para configurar o filtro do Pulmão
+    btn_pulmao = Button(window, text="Filtro para o Pulmão", command=lambda: toggle_state(True))
+    btn_pulmao.pack(pady=5)
+
+    # Botão para configurar o filtro do Coração
+    btn_coracao = Button(window, text="Filtro para o Coração", command=lambda: toggle_state(False))
+    btn_coracao.pack(pady=5)
+
+    # Botão para iniciar o processo principal
+    btn_start = Button(window, text="Começar gravação", command=main)
+    btn_start.pack(pady=20)
+
+    window.mainloop()
 
 if __name__ == "__main__":
-    main()
+    create_gui()
