606e22b24fd6b67c0e11c89916b6dda5b5d5730f
Zenni <marqueszenivitor@gmail.com>
2024-09-30 11:31:42 -0300

    Coeficientes para floating point

--- ARQUIVOS_PY_ARM/calc_coef.py
+++ ARQUIVOS_PY_ARM/calc_coef.py
@@ -1,18 +1,18 @@
 from scipy.signal import cheby2
 
 # Definir a frequência de corte (faixa de 15 Hz a 250 Hz)
-lowcut = 100.0
-highcut = 800.0
+lowcut = 15.0
+highcut = 250.0
 fs = 8000.0  # Taxa de amostragem
 
 # Definir a ordem do filtro e a atenuação fora da banda passante
-ordem = 20
+secoes = 20
 rs = 80  # Atenuação na banda de rejeição em dB
 
 # Gerar coeficientes usando um filtro Chebyshev Tipo II de ordem 40
-sos = cheby2(ordem, rs, [lowcut, highcut], btype='band', fs=fs, output='sos')
+sos = cheby2(secoes, rs, [lowcut, highcut], btype='band', fs=fs, output='sos')
 
 # Formatar e imprimir apenas b0, b1, b2, a1, a2 com no máximo 10 casas decimais para cada seção
 for section in sos:
     b0, b1, b2, a0, a1, a2 = section
-    print(f'{b0:.7f}, {b1:.7f}, {b2:.7f}, {-a1:.7f}, {-a2:.7f},')
+    print(f'{b0:.32f}, {b1:.32f}, {b2:.32f}, {-a1:.32f}, {-a2:.32f},')
